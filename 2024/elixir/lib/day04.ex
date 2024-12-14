7445ea527ab337349713a21d6e0851b6574854e6
kik4444 <7779637+kik4444@users.noreply.github.com>
2024-12-14 13:09:07 +0200

    Do day04

---
+++ 2024/elixir/lib/day04.ex
@@ -0,0 +1,69 @@
+defmodule Aoc2024.Day04 do
+  def part1(path) do
+    {grid, width, height} = parse(path)
+
+    # "d" means delta.
+    for row <- width,
+        col <- height,
+        drow <- -1..1,
+        dcol <- -1..1,
+        (row + drow * 3) in width,
+        (col + dcol * 3) in height,
+        at(grid, row, col) == "X",
+        reduce: 0 do
+      acc ->
+        acc + if xmas?(grid, row, col, drow, dcol), do: 1, else: 0
+    end
+  end
+
+  def part2(path) do
+    {grid, width, height} = parse(path)
+
+    # Not interested in "A"s at the start or end of a row or column
+    # because they can't have any neighbors.
+    for row <- width,
+        col <- height,
+        (row + 1) in width and (row - 1) in width,
+        (col + 1) in height and (col - 1) in height,
+        at(grid, row, col) == "A",
+        reduce: 0 do
+      acc ->
+        acc + if x_mas?(grid, row, col), do: 1, else: 0
+    end
+  end
+
+  def parse(path) do
+    grid =
+      File.read!(path)
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn line -> String.graphemes(line) |> List.to_tuple() end)
+      |> List.to_tuple()
+
+    width = tuple_size(elem(grid, 0))
+    height = tuple_size(grid)
+
+    {grid, 0..(width - 1), 0..(height - 1)}
+  end
+
+  def at(grid, row, col), do: grid |> elem(col) |> elem(row)
+
+  def xmas?(grid, row, col, drow, dcol) do
+    Enum.map(1..3, &at(grid, row + &1 * drow, col + &1 * dcol)) == ~w[M A S]
+  end
+
+  def x_mas?(grid, row, col) do
+    backward_diagonal =
+      Enum.sort([
+        at(grid, row + 1, col + 1),
+        at(grid, row - 1, col - 1)
+      ])
+
+    forward_diagonal =
+      Enum.sort([
+        at(grid, row + 1, col - 1),
+        at(grid, row - 1, col + 1)
+      ])
+
+    forward_diagonal == ~w[M S] and backward_diagonal == ~w[M S]
+  end
+end
