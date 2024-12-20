1b3a16bc9aed4ac8ad749899548022cd1b693f88
kik4444 <7779637+kik4444@users.noreply.github.com>
2024-12-20 18:08:02 +0200

    Finish day 06 and refactor

--- 2024/elixir/lib/day06.ex
+++ 2024/elixir/lib/day06.ex
@@ -7,45 +7,71 @@ defmodule Aoc2024.Day06 do
 
   def part1(input) do
     %Lab{} = lab = parse(input)
-    uniq_pos = MapSet.new()
 
-    walk(lab, uniq_pos) |> MapSet.size()
+    walk(lab, MapSet.new())
+    |> then(fn {:exit, positions} -> positions end)
+    |> Stream.uniq_by(fn {x, y, _dir} -> {x, y} end)
+    |> Enum.count()
+  end
+
+  def part2(input) do
+    %Lab{} = lab = parse(input)
+
+    # Add a new obstacle to each unique position the guard has been in,
+    # then check if that creates any loops.
+    walk(lab, MapSet.new())
+    |> then(fn {:exit, positions} -> positions end)
+    |> Stream.uniq_by(fn {x, y, _dir} -> {x, y} end)
+    |> Task.async_stream(fn {x, y, _dir} ->
+      row = elem(lab.grid, y)
+      new_row = put_elem(row, x, "#")
+      new_grid = put_elem(lab.grid, y, new_row)
+
+      walk(put_in(lab.grid, new_grid), MapSet.new())
+    end)
+    |> Enum.count(fn {:ok, {break_type, _}} -> break_type == :loop end)
   end
 
   def walk(%Lab{} = lab, %MapSet{} = uniq_pos) do
-    uniq_pos = MapSet.put(uniq_pos, {lab.x, lab.y})
+    entry = {lab.x, lab.y, lab.direction}
 
-    look_around(lab, uniq_pos)
+    # Check for loops.
+    # A loop is when we've already visited a given spot with the same direction.
+    if MapSet.member?(uniq_pos, entry) do
+      {:loop, uniq_pos}
+    else
+      look_around(lab, MapSet.put(uniq_pos, entry))
+    end
   end
 
   def look_around(%Lab{} = lab, uniq_pos) do
     case lab.direction do
-      :up when lab.y == 0 -> uniq_pos
-      :right when lab.x == lab.width -> uniq_pos
-      :down when lab.y == lab.height -> uniq_pos
-      :left when lab.x == 0 -> uniq_pos
+      :up when lab.y == 0 -> {:exit, uniq_pos}
+      :right when lab.x == lab.width -> {:exit, uniq_pos}
+      :down when lab.y == lab.height -> {:exit, uniq_pos}
+      :left when lab.x == 0 -> {:exit, uniq_pos}
       _ -> move(lab, uniq_pos)
     end
   end
 
   def move(%Lab{direction: :up} = lab, uniq_pos) do
     next_cell = at(lab.grid, lab.x, lab.y - 1)
-    if next_cell == "#", do: turn_right(lab, uniq_pos), else: walk(%Lab{lab | y: lab.y - 1}, uniq_pos)
+    if next_cell == "#", do: turn_right(lab, uniq_pos), else: walk(put_in(lab.y, lab.y - 1), uniq_pos)
   end
 
   def move(%Lab{direction: :right} = lab, uniq_pos) do
     next_cell = at(lab.grid, lab.x + 1, lab.y)
-    if next_cell == "#", do: turn_right(lab, uniq_pos), else: walk(%Lab{lab | x: lab.x + 1}, uniq_pos)
+    if next_cell == "#", do: turn_right(lab, uniq_pos), else: walk(put_in(lab.x, lab.x + 1), uniq_pos)
   end
 
   def move(%Lab{direction: :down} = lab, uniq_pos) do
     next_cell = at(lab.grid, lab.x, lab.y + 1)
-    if next_cell == "#", do: turn_right(lab, uniq_pos), else: walk(%Lab{lab | y: lab.y + 1}, uniq_pos)
+    if next_cell == "#", do: turn_right(lab, uniq_pos), else: walk(put_in(lab.y, lab.y + 1), uniq_pos)
   end
 
   def move(%Lab{direction: :left} = lab, uniq_pos) do
     next_cell = at(lab.grid, lab.x - 1, lab.y)
-    if next_cell == "#", do: turn_right(lab, uniq_pos), else: walk(%Lab{lab | x: lab.x - 1}, uniq_pos)
+    if next_cell == "#", do: turn_right(lab, uniq_pos), else: walk(put_in(lab.x, lab.x - 1), uniq_pos)
   end
 
   def turn_right(%Lab{direction: :up} = lab, uniq_pos), do: walk(put_in(lab.direction, :right), uniq_pos)
